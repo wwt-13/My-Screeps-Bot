043c47a949f8accb2d6c69b44d580494c631bd00
GG <qq1270414897@gmail.com>
2023-09-14 16:18:14 +0800

    提交测试

--- src/main.js
+++ src/main.js
@@ -1,20 +1,17 @@
 // 游戏入口函数
 // 引入外部依赖
-import { run } from "./modules/harvester";
+import { run } from './modules/harvester';
 
 export const loop = function () {
     // 查看Spawn1的能量剩余
-    console.log(Game.spawns["Spawn1"].store[RESOURCE_ENERGY]);
+    console.log(Game.spawns['Spawn1'].store[RESOURCE_ENERGY]);
     // 如果能量充足，则不停创建Harvest
-    if (Game.spawns["Spawn1"].store[RESOURCE_ENERGY] > 200) {
+    if (Game.spawns['Spawn1'].store[RESOURCE_ENERGY] > 200) {
         let index = Object.keys(Game.creeps).length;
-        while (Game.creeps["Harvester" + index]) {
+        while (Game.creeps['Harvester' + index]) {
             index++;
         }
-        Game.spawns["Spawn1"].spawnCreep(
-            [WORK, CARRY, MOVE],
-            "Harvester" + index
-        );
+        Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Harvester' + index);
     }
     for (let name in Game.creeps) {
         let creep = Game.creeps[name];
