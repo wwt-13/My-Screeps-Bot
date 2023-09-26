14fb0ebe248c305dd0ab5f3f62666bc8d4f034fc
GG <qq1270414897@gmail.com>
2023-09-26 12:52:20 +0800

    rollup初步

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,16 @@
 // import clear from "rollup-plugin-clear";
 
 // tell rollup what it needs to do
+// rollup是一个构建工具
+// 给他一堆源代码，他就可以把源代码构建成一个可以在浏览器中运行的文件
 export default {
     // 源代码入口
-    input: "src/main.js",
+    input: 'src/main.js',
     // 构建产物配置
     output: {
         // 输出到哪个文件
-        file: "dist/main.js",
-        format: "cjs",
+        file: 'dist/main.js',
+        format: 'cjs',
         sourcemap: true,
     },
 };
